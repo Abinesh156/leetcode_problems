ed4f9ca5cf6827c869e21b76cdc55e8b5f321300
Abinesh156 <s.abinesh102@gmail.com>
2025-10-24 16:40:15 +0530

    24/10/25

---
+++ 14. Longest Common Prefix/main.py
@@ -0,0 +1,5 @@
+strs = ["flower","flow","flight"]
+j=0
+for i in strs[0]:
+    i.join(j)
+    print(j)
---
+++ 9. Palindrome Number/main.py
@@ -0,0 +1,7 @@
+
+x = 121
+if "".join(reversed(str(x)))==str(x):
+    print(True)
+else:
+    print(False)
+print("".join(reversed(str(x))))
