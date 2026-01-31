9bb3103d7ea5b04e43b963fd7b9a0cb37dc6ce76
abinesh156 <s.abinesh102@gmail.com>
2026-01-31 12:06:36 +0530

    31-01-2026

---
+++ 1984. Minimum Difference Between Highest and Lowest of K Scores/main.py
@@ -0,0 +1,5 @@
+nums = [9,4,1,7]
+k = 3
+for i in range(len(nums)-1):
+    for j in range(k):
+        print(nums[i])
--- 66. Plus One/main.py
+++ 66. Plus One/main.py
@@ -1,6 +1,10 @@
 digits = [1,2,3]
-a="".join(map(str, digits))
-a=int(a)+1
-a = [int(d) for d in str(a)]  
+# a="".join(map(str, digits))
+# a=int(a)+1
+# a = [int(d) for d in str(a)]  
 
-print(a)
+# print(a)
+
+if len(digits)<=1:
+    digits=digits[0]+1
+    print(digits)
